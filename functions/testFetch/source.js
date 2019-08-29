3e35a523b2c437804f1d885a9d1a5f0d02ba7cd8
Pete Williamson <peter.williamson@mongodb.com>
2019-08-29 20:22:46 +0100

    Remove test functions

--- functions/testFetch/source.js
+++
@@ -1,9 +0,0 @@
-exports = function(arg){
-  const mongodb = context.services.get("eventArchiver");
-  const itemsCollection = mongodb.db("coinbase").collection("testapp");
-  return itemsCollection.findOne({})
-  .then(result => {
-    console.log(EJSON.stringify(result));
-    return(result);
-  } );
-};
